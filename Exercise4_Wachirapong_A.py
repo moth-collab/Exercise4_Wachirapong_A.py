e003a31b5beadb2669299def89466ecaa248de64
moth-collab <moth@finnomena.com>
2025-11-16 09:45:45 +0700

    Add files via upload

---
+++ Exercise4_Wachirapong_A.py
@@ -0,0 +1,10 @@
+foundation_english_score = 85.5
+general_business_score = 78.0
+intro_to_computer_systems_score = 92.5
+computer_programming_score = 95.0
+
+print("--- Your Score ---")
+print(f"Foundation English : {foundation_english_score}")
+print(f"General Business : {general_business_score}")
+print(f"Introduction to Computer Systems : {intro_to_computer_systems_score}")
+print(f"Computer Programming : {computer_programming_score}")
